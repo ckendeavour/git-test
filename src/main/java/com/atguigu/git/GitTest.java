e6e1c174b399696e54c08a12325e16576cd4533e
cuikk <cuikk@qq.com>
2021-05-17 17:25:38 +0800

    idea first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author cuikk
+ * @create 2021-05-17 17:22
+ * @description
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!");
+    }
+}
